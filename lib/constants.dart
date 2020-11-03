5f927e0bb4639cce60ca6fd019d79dc92bd52557
Jefferson Yure <yure.siilva9@gmail.com>
2020-11-02 22:25:51 -0300

    implemetando design

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+double bordas = 5;
+
 const backgrounColor = Color(0xff0a8754);
 const primaryColor = Color(0xffbfd7ea);
 const secondaryColor = Colors.white,
@@ -17,6 +19,13 @@ const subtitulo = TextStyle(
 
 const textonormal = TextStyle(
   fontWeight: FontWeight.bold,
-  color: Colors.grey,
+  color: Colors.black45,
   fontSize: 12,
+);
+
+//elevção de um Container
+const elevation = BoxShadow(
+  offset: Offset(0, 15),
+  blurRadius: 27,
+  color: Colors.black12, // Black color with 12% opacity
 );
--- lib/listadeempresas.dart
+++ lib/listadeempresas.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:exampleflutter/Page2.dart';
 import 'package:flutter/rendering.dart';
 import 'package:exampleflutter/constants.dart';
-import 'package:exampleflutter/search_box.dart';
 
 class Listadeempresas extends StatefulWidget {
   @override
@@ -11,6 +10,9 @@ class Listadeempresas extends StatefulWidget {
 }
 
 class _ListadeempresasState extends State<Listadeempresas> {
+  int selectedIndex = 0;
+  List categoria = ["Esportes", "Mercado", "Bar", "restaurante"];
+  List categories = ["Esportes", "Mercado", "Bar", "Restaurante"];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,6 +33,7 @@ class _ListadeempresasState extends State<Listadeempresas> {
       ),
       body: Column(
         children: <Widget>[
+//barra de pesquisa.....................................................................................
           Container(
             margin: EdgeInsets.all(20.0),
             padding: EdgeInsets.symmetric(
@@ -41,12 +44,116 @@ class _ListadeempresasState extends State<Listadeempresas> {
                 color: Colors.white.withOpacity(0.4),
                 borderRadius: BorderRadius.circular(5)),
             child: TextField(
-              decoration: InputDecoration(icon: Icon(Icons.search)),
+              decoration: InputDecoration(
+                  enabledBorder: InputBorder.none,
+                  focusedBorder: InputBorder.none,
+                  icon: Icon(Icons.search),
+                  hintText: "Pesquisar Empresa",
+                  hintStyle: TextStyle(color: Colors.black26)),
             ),
-          )
+          ),
+//lista de na vertical.......................................................................................
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 20 / 2),
+            height: 35,
+            child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: categoria.length,
+                itemBuilder: (context, index) => GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          selectedIndex = index;
+                        });
+                      },
+                      child: Container(
+                        alignment: Alignment
+                            .center, //alinhando os widgets presentes no container
+                        margin: EdgeInsets.only(
+                            left: 20.0,
+                            right: index == categoria.length - 1 ? 20.0 : 0),
+                        padding: EdgeInsets.symmetric(horizontal: 20.0),
+                        decoration: BoxDecoration(
+                            color: index == selectedIndex
+                                ? backgrounColor
+                                : Colors.white.withOpacity(0.4),
+                            borderRadius: BorderRadius.circular(5)),
+                        child: Text(
+                          categoria[index],
+                          style: textonormal,
+                        ),
+                      ),
+                    )),
+          ),
+          Divider(
+            height: 20,
+            color: Colors.transparent,
+          ),
+//desgn do body...............................................................................
+          Expanded(
+              child: Stack(
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.only(top: 70),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30),
+                    )),
+              ),
+//............................................
+
+              ProductCard()
+            ],
+          ))
         ],
       ),
-      /*appBar: AppBar(
+    );
+  }
+}
+
+class ProductCard extends StatelessWidget {
+  const ProductCard({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 20, vertical: 20 / 2),
+      height: 160,
+      child: Stack(
+        alignment: Alignment.bottomCenter,
+        children: <Widget>[
+          Container(
+            height: 136,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(22), color: backgrounColor),
+            child: Container(
+              margin: EdgeInsets.only(left: 10),
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  boxShadow: [elevation],
+                  borderRadius: BorderRadius.circular(22)),
+            ),
+          ),
+          Positioned(
+            child: Container(
+              padding: EdgeInsets.symmetric(
+                horizontal: 20,
+              ),
+              height: 160,
+              width: 200,
+              child: Image.asset('images/mixicon.jpg'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+/*appBar: AppBar(
           elevation: 1,
           title: Text(
             "Empresas",
@@ -64,7 +171,7 @@ class _ListadeempresasState extends State<Listadeempresas> {
             decoration: BoxDecoration(color: Colors.white)
           )*/
 
-      /* Card(
+/* Card(
               elevation: 5,
               child: InkWell(
                 onTap: () {
@@ -166,6 +273,3 @@ class _ListadeempresasState extends State<Listadeempresas> {
             ),
         ],
       ),*/
-    );
-  }
-}
